f1283da5d627cf534bc8aa9fdc80ae2bbeba95e0
vime <vime@sofico.be>
2020-11-09 17:44:48 -0600

    Create battle module

---
+++ src/app/modules/battle/battle.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BattleRoutingModule } from './battle-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BattleRoutingModule
+  ]
+})
+export class BattleModule { }
